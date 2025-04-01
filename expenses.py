0b35418f73c80dda0d1ae3e7a40cfc64f05a31da
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-04-01 14:45:13 -0500

    Improve second solution in expenses exercise using range

--- expenses.py
+++ expenses.py
@@ -7,6 +7,13 @@ expenses = [10.50, 8, 5, 15, 20, 5, 3]
 # for x in expenses:
 #   sum = sum + x
 
+total = 0
+expenses = []
+num_expenses = int(input("Enter the # of expenses: "))
+
+for i in range(num_expenses):
+  expenses.append(float(input("Enter an expense: ")))
+  
 total = sum(expenses) # Python's built-in sum function
 
 print("You spent $", total, sep='')
